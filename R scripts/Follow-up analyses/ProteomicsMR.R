56804ac3cdd9230a8472a55ef4b539756f74d06e
emilymorbey <152413636+emilymorbey@users.noreply.github.com>
2024-05-31 14:34:57 +0100

    for loop structure for proteomics

---
+++ R scripts/Follow-up analyses/ProteomicsMR.R
@@ -0,0 +1,62 @@
+library(tidyverse)
+library(MendelianRandomization)
+library(data.table)
+library(readr)
+
+
+setwd("C:/Users/emorb/OneDrive - University of Cambridge/PhD/MR/Testosterone_CAD_MR/Testosterone CAD MR R files/TestosteroneCAD/Proteomics")
+
+
+files <- read.table("Males_OLINK.csv", header=TRUE)
+
+SNPs <- read.csv("Testosterone_SNPs_OL.csv", header = TRUE)
+
+
+
+
+# Initialize an empty dataframe to store the results
+results_df <- data.frame(File = character(), Estimate = numeric(), P_Value = numeric(), stringsAsFactors = FALSE)
+
+# Loop over each file in the list
+for(i in seq_along(files[,1])) {
+  # Read the file
+  T <- read.table(files[i,1], header = TRUE)
+  
+  # Merge with the SNP data
+  merged_data <- merge(SNPs, T, by = "ID")
+  
+  # Select relevant columns 
+  allele_matching <- select(merged_data, "ID", "Trait_raising", 
+                            "Other_allele", "Weight", "SE_weight", 
+                            "MarkerName", "ALLELE0", "ALLELE1",
+                            "BETA", "SE")
+  
+  # Harmonizing so the effect alleles for CAD and SHBG are the same
+  # Changing the betas here 
+  allele_matching$BETA <- as.numeric(allele_matching$BETA)
+  allele_matching$HARM_BETA <- if_else(allele_matching$Trait_raising != allele_matching$ALLELE0,
+                                       allele_matching$BETA * -1, allele_matching$BETA)
+  
+  # Running the model 
+  allele_matching$SE <- as.numeric(allele_matching$SE)
+  IVW_weights <- allele_matching$SE^-2 
+  inverse_weighted_LR <- lm(allele_matching$HARM_BETA ~ allele_matching$Weight - 1, weights = IVW_weights) 
+  
+  # Get the summary of the model
+  summary_model <- summary(inverse_weighted_LR)
+  
+  # Extract the estimate and p-value
+  estimate <- summary_model$coefficients[1, "Estimate"]
+  p_value <- summary_model$coefficients[1, "Pr(>|t|)"]
+  
+  # Add the results to the dataframe
+  results_df <- rbind(results_df, data.frame(File = files[i, 1], Estimate = estimate, P_Value = p_value, stringsAsFactors = FALSE))
+}
+
+# Print the results
+print(results_df)
+
+
+
+
+
